3ad8569ce2e6c349ddd999a05283ab2dc951a729
kibetamos <amos.rotich@strathmore.edu>
2025-05-12 18:21:14 +0300

    added reiews project

--- 03-reviews/app.js
+++ 03-reviews/app.js
@@ -31,8 +31,61 @@ const reviews = [
 ];
 // select items
 const img = document.getElementById('person-img');
+const author = document.getElementById('author');
 const job = document.getElementById('job');
 const info = document.getElementById('info');
-const author = document.getElementById('author');
 
 
+
+const prevBtn = document.querySelector('.prev-btn');
+const nextBtn = document.querySelector('.next-btn');
+const randomBtn = document.querySelector('.random-btn');
+
+//set starting item\
+
+let currentItem = 0;
+
+//loadinitila content
+
+window.addEventListener("DOMContentLoaded", function (){
+  showPerson(currentItem);
+  // console.log("shake and bake");
+});
+ //show pwerson based on item
+
+ function showPerson(){
+  const item = reviews[currentItem];
+  author.textContent = item.name;
+  img.src= item.img;
+  job.textContent = item.job;
+  info.textContent = item.text;
+ }
+
+//show next person
+
+nextBtn.addEventListener('click', function(){
+  currentItem++;
+  if(currentItem > reviews.length -1){
+    currentItem = 0;
+  }
+  showPerson();
+
+});
+
+//show prev person
+prevBtn.addEventListener('click', function(){
+  currentItem--;
+  if(currentItem < 0){
+    currentItem = reviews.length-1;
+  }
+  showPerson();
+
+});
+
+//show random person
+
+randomBtn.addEventListener('click', function(){
+currentItem = Math.floor(Math.random() * reviews.length);
+// console.log(showPerson)
+showPerson();
+});
